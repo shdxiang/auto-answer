cc989c2995ee7f92dac42d1a2159c56f9e5cff3b
shdxiang <shdxiang@qq.com>
2018-01-10 22:49:15 +0800

    it works

---
+++ main.py
@@ -0,0 +1,100 @@
+import logging
+import subprocess
+from pynput import keyboard
+
+HOT_KEY = keyboard.Key.shift
+
+
+# sox -d -p | sox -p -b 16 -r 16000 recording.wav remix -
+
+def start_record():
+    global g_remix
+    global g_record
+
+    logging.debug('recording...')
+
+    args = 'sox -p -b 16 -r 16000 recording.wav remix -'.split()
+    g_remix = subprocess.Popen(args, stdin=subprocess.PIPE)
+
+    args = 'sox -d -p'.split()
+    g_record = subprocess.Popen(args, stdout=g_remix.stdin)
+
+
+def stop_record():
+    global g_remix
+    global g_record
+
+    g_record.terminate()
+    g_remix.terminate()
+
+
+# tar -czf - recording.wav | ssh root@wechat.szfda.cn
+# "tar -xzf - -C ~/answer/auto-answer; cd answer/auto-answer; ./recognize.sh"
+
+
+def recognize():
+    global g_tar
+    global g_ssh
+
+    logging.debug('recognizing...')
+
+    args = ['ssh', 'root@wechat.szfda.cn',
+            'tar -xzf - -C ~/answer/auto-answer; cd answer/auto-answer; ./recognize.sh']
+    g_ssh = subprocess.Popen(
+        args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+    args = 'tar -czf - recording.wav'.split()
+    g_tar = subprocess.Popen(args, stdout=g_ssh.stdin)
+
+    try:
+        outs, errs = g_ssh.communicate(timeout=5)
+
+        if errs:
+            return None
+        if outs:
+            return outs
+    except subprocess.TimeoutExpired:
+        g_ssh.terminate()
+        return None
+
+
+# curl -L -o web/index.html http://www.baidu.com/s?wd=
+def query(data):
+    url = 'http://www.baidu.com/s?wd=' + data
+    args = 'curl -L -o web/index.html'.split()
+    args.append(url[:-1])
+    logging.debug(args)
+
+    g_curl = subprocess.Popen(args)
+    g_curl.wait()
+
+
+def on_key_press(key):
+    # logging.debug(key)
+    if key == HOT_KEY:
+        start_record()
+
+
+def on_key_release(key):
+    # logging.debug(key)
+    if key == HOT_KEY:
+        stop_record()
+
+        result = recognize()
+        if result:
+            data = result.decode('utf-8')
+            logging.debug(data)
+            query(data)
+
+
+def main():
+    logFormat = '%(asctime)s %(filename)s [%(lineno)d][%(levelname)s] %(message)s'
+    logging.basicConfig(level=logging.DEBUG, format=logFormat)
+
+    with keyboard.Listener(on_press=on_key_press,
+                           on_release=on_key_release) as listener:
+        listener.join()
+
+
+if __name__ == '__main__':
+    main()

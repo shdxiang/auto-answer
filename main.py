278f20ef2be866f689841088406c3f01213f5c06
shdxiang <shdxiang@qq.com>
2018-01-11 21:42:38 +0800

    improve query

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import logging
 import subprocess
+import urllib.parse
 from pynput import keyboard
+
 
 HOT_KEY = keyboard.Key.shift
 
 REMIX = 'sox -p -b 16 -r 16000 data/audio.wav remix -'
 RECORD = 'sox -d -p'
 RECOGNIZE = 'docker exec auto-answer recognizer /root/data/audio.wav'
-QUERY_URL = 'http://www.baidu.com/s?wd={}'
+QUERY_URL = 'http://www.baidu.com/s?ie=utf-8&wd={}'
 QUERY = 'curl -L -o web/index.html'
 
 
@@ -51,37 +53,45 @@ def recognize():
 
 
 def query(data):
+    transtab = str.maketrans("，。？！", "    ")
+    data = data.translate(transtab)
+
+    data = urllib.parse.quote(data.strip())
+
+    logging.debug(data)
+
     url = QUERY_URL.format(data)
-    # logging.debug(url)
+    logging.info(url)
+
     args = QUERY.split()
-    args.append(url[:-1])
-    # logging.debug(args)
+    args.append(url)
+    logging.debug(args)
 
     g_curl = subprocess.Popen(args)
     g_curl.wait()
 
 
 def on_key_press(key):
-    # logging.debug(key)
+    logging.debug(key)
     if key == HOT_KEY:
         start_record()
 
 
 def on_key_release(key):
-    # logging.debug(key)
+    logging.debug(key)
     if key == HOT_KEY:
         stop_record()
 
         result = recognize()
         if result:
             data = result.decode('utf-8')
-            logging.debug(data)
+            logging.info(data)
             query(data)
 
 
 def main():
     logFormat = '%(asctime)s %(filename)s [%(lineno)d][%(levelname)s] %(message)s'
-    logging.basicConfig(level=logging.DEBUG, format=logFormat)
+    logging.basicConfig(level=logging.INFO, format=logFormat)
 
     with keyboard.Listener(on_press=on_key_press,
                            on_release=on_key_release) as listener:
